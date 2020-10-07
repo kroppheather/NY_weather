9075d1fbaa21fa22051e6e2379e20c6bade7cce3
Abby Roller <aroller@hamilton.edu>
2020-10-06 20:12:03 -0400

    read in prcp,tmax,tmin csv files

--- data_analysis.R
+++ data_analysis.R
@@ -1,5 +1,12 @@
 
-source("/Users/hkropp/Documents/GitHub/NY_weather/data_organize_script.r")
+#source("/Users/hkropp/Documents/GitHub/NY_weather/data_organize_script.r")
 
 
-PrcpData
+#read in prcp data
+PRCP <- read.csv("/Users/abby/Documents/NYweather/prcp_all.csv")
+
+#read in tmax data
+TMAX <- read.csv("/Users/abby/Documents/NYweather/Tmax_all.csv")
+
+#read in tmin data
+TMIN <- read.csv("/Users/abby/Documents/NYweather/Tmin_all.csv")
