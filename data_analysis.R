702090f42621657c7db046444c01aa8a32a174a5
Rachel Pike <rachelpike@rachels-mbp-2.hamilton.edu>
2020-10-06 20:13:14 -0400

    started reading in data

--- data_analysis.R
+++ data_analysis.R
@@ -1,5 +1,13 @@
+# Rachel Started 10/6
 
-source("/Users/hkropp/Documents/GitHub/NY_weather/data_organize_script.r")
+# source("/Users/hkropp/Documents/GitHub/NY_weather/data_organize_script.r")
 
+# Reading in prcp data from google drive
+PrcpData <- read.csv("/Volumes/GoogleDrive/.shortcut-targets-by-id/10ARTNFd7_vF4j5cC_nYlyqrsTjmLzCsj/NYweather/data/prcp_all.csv")
 
-PrcpData
+# Reading in tmax data from google drive
+TmaxData <- read.csv("/Volumes/GoogleDrive/.shortcut-targets-by-id/10ARTNFd7_vF4j5cC_nYlyqrsTjmLzCsj/NYweather/data/Tmax_all.csv")
+
+# Reading in tmin data from google drive
+TminData <- read.csv("/Volumes/GoogleDrive/.shortcut-targets-by-id/10ARTNFd7_vF4j5cC_nYlyqrsTjmLzCsj/NYweather/data/Tmin_all.csv")
+
